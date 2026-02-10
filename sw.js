8895840abf4649ebb34cce15bb28e10eac5e6771
yaroslavdidyk12-oss <yaroslavdidyk12@gmail.com>
2026-02-10 18:03:16 +0100

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-self.addEventListener("install", e => {
- e.waitUntil(
-  caches.open("city-game").then(cache => {
-    return cache.addAll(["./","./index.html","./manifest.json"]);
-  })
- );
+const CACHE_NAME = 'miasto-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './index-pl.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {
- e.respondWith(
-  caches.match(e.request).then(res => res || fetch(e.request))
- );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
