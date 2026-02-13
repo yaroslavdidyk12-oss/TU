18f6ef379db08c289d1eb749826ae0a49220aef2
yaroslavdidyk12-oss <yaroslavdidyk12@gmail.com>
2026-02-13 08:03:04 +0100

    Add icons to cache in service worker

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
 const CACHE_NAME = "urbanist-v1";
-const urlsToCache = [
+
+const FILES_TO_CACHE = [
   "index.html",
-  "manifest.json"
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
